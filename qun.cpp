b639ed22cf545f08b3cd98d30ba61833057a300d
3444790553 <118905240+3444790553@users.noreply.github.com>
2023-08-14 22:19:47 +0800

    Add files via upload

---
+++ qun.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    int t;
+    int n;
+    string res[2];
+    int x = 0;
+    cin >> t;
+    int q = t;
+    int flag = 1;
+    while (t--) {
+        cin >> n;
+        int i = 0;
+        string pan[10000];
+        string pp;
+        while (n--) {
+            cin >> pp;
+            if (pp!= "PERFECT") 
+                flag = 0;    }
+
+
+        if (flag == 1)
+            res[x] = "MILLION Master";
+        else
+            res[x] = "NA Noob";
+        x++;
+        flag = 1;
+    }
+    int j = 0;
+    while (q--) {
+        cout << res[j] << endl;
+
+        j++;
+    }   return 0;
+
+}
